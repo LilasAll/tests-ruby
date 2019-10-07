df6cd060dd5482d80d8a7ac7898cd106e1a4f051
Lilas <55747074+LilasAll@users.noreply.github.com>
2019-10-07 17:12:53 +0200

    Add files via upload

--- spec/02_calculator_spec.rb
+++ spec/02_calculator_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/02_calculator'
 
 describe "add" do
+
   it "adds 0 and 0" do
     expect(add(0,0)).to eq(0)
   end
@@ -12,15 +13,19 @@ describe "add" do
   it "adds positive numbers" do
     expect(add(2,6)).to eq(8)
   end
+
 end
 
 describe "subtract" do
+
   it "subtracts numbers" do
     expect(subtract(10,4)).to eq(6)
   end
+
 end
 
 describe "sum" do
+
   it "computes the sum of an empty array" do
     expect(sum([])).to eq(0)
   end
@@ -36,6 +41,7 @@ describe "sum" do
   it "computes the sum of an array of many numbers" do
     expect(sum([1,3,5,7,9])).to eq(25)
   end
+
 end
 
 # Let's write some tests !
@@ -47,23 +53,45 @@ describe "#multiply" do
     expect(multiply(3, 4)).to eq(12)
   end
 
-  it "multiplies two other numbers"
-    # one other test here, don't forget do end ;)
+  it "multiplies two other numbers" do
+    expect(multiply(5, 5)).to eq(25)
+  end
 
-  it "multiplies two other numbers, one of them being 0"
-    # one last test, with 0 in it
+  it "multiplies two other numbers, one of them being 0" do
+    expect(multiply(3, 0)).to eq(0)
+  end
 
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+
+  it "raises one number to the power of another number" do 
+    expect(power(2, 2)).to eq(4)
+  end
+
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+
+  it "computes the factorial of 0" do
+    expect(factorial(0)).to eq(1)
+  end
+
+  it "computes the factorial of 1" do
+    expect(factorial(1)).to eq(1)
+  end
+
+  it "computes the factorial of 2" do 
+    expect(factorial(2)).to eq(2)
+  end
+
+  it "computes the factorial of 5" do
+    expect(factorial(5)).to eq(120)
+  end
+
+  it "computes the factorial of 10" do
+    expect(factorial(10)).to eq(3628800)
+  end
+
 end
